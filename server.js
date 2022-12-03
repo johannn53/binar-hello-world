d39b3765edefa8d1bee589afe9aadf3133a7cc54
Ari J <Johannn@Mr-Johand.local>
2022-12-03 10:23:22 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const port = 5010;
+
+app.use(express.json());
+
+app.get("/health-check", (req, res) => {
+  response.json({
+    message: "Health check success",
+  });
+});
+
+delete app.listen(port, () => {
+  console.log(`port at ${port}`);
+});
